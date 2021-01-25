8e9f4cd4dd001c5155e6db075af76d6de484d00d
Melanie Panem <melanielynnpanem@gmail.com>
2021-01-24 20:50:17 -0800

    Updates

---
+++ precache-manifest.79d5e0fe1e9e6b6ea65c6d211276b938.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297e51e4f423b3baae2a592de904af20",
+    "url": "/mogwais-practice-reactjs/index.html"
+  },
+  {
+    "revision": "f6ae5c33795b2bdcd9a6",
+    "url": "/mogwais-practice-reactjs/static/js/2.a3778911.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mogwais-practice-reactjs/static/js/2.a3778911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb317d7c80a970e50e9",
+    "url": "/mogwais-practice-reactjs/static/js/main.5d4972be.chunk.js"
+  },
+  {
+    "revision": "122f89c670709a692dc5",
+    "url": "/mogwais-practice-reactjs/static/js/runtime-main.fe8feb76.js"
+  },
+  {
+    "revision": "67fdd64c862eab68c87eb5ce3cc79870",
+    "url": "/mogwais-practice-reactjs/static/media/gremlin-01.67fdd64c.png"
+  },
+  {
+    "revision": "85fa2f79edd48d349718488332a09602",
+    "url": "/mogwais-practice-reactjs/static/media/gremlin-02.85fa2f79.png"
+  },
+  {
+    "revision": "f917f2d231ee09ddb6cdbfd90f6d0570",
+    "url": "/mogwais-practice-reactjs/static/media/gremlin-03.f917f2d2.png"
+  },
+  {
+    "revision": "9532329d77025a192e3d9523fadaf1c1",
+    "url": "/mogwais-practice-reactjs/static/media/gremlin-04.9532329d.png"
+  },
+  {
+    "revision": "7399cc6a94f31770a93d97803b8b7000",
+    "url": "/mogwais-practice-reactjs/static/media/v-mogwai-color-03.7399cc6a.svg"
+  },
+  {
+    "revision": "26f71fb06ea85e632ddd9404fbbf9105",
+    "url": "/mogwais-practice-reactjs/static/media/v-mogwai-color-04.26f71fb0.svg"
+  },
+  {
+    "revision": "3f1b7e9b9a351f52b30c9d7174e392df",
+    "url": "/mogwais-practice-reactjs/static/media/v-mogwai-color-05.3f1b7e9b.svg"
+  },
+  {
+    "revision": "3a9bc0b6cb9a7aa38d46d7f8589b70dc",
+    "url": "/mogwais-practice-reactjs/static/media/v-mogwai-color-06.3a9bc0b6.svg"
+  },
+  {
+    "revision": "7982257be8997cde038b2b848042736b",
+    "url": "/mogwais-practice-reactjs/static/media/v-mogwai-color-07.7982257b.svg"
+  },
+  {
+    "revision": "0a02c3d5741502b8d5f3c9c0fdbd7e1c",
+    "url": "/mogwais-practice-reactjs/static/media/v-mogwai-outline-01.0a02c3d5.svg"
+  }
+]);
